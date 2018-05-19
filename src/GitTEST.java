0627816614d29a786d49c47a56b2c69452169780
AdvancerZhang <chenzhang0626@126.com>
2018-05-19 22:25:32 +0800

    Delete useless annotation.

--- src/GitTEST.java
+++ src/GitTEST.java
@@ -1,16 +1,5 @@
-import javax.swing.plaf.synth.SynthSpinnerUI;
-
 public class GitTEST {
-
 	public static void main(String[] args) {
-		System.out.println("Hello,Git.");
-		System.out.println("Nanjing.China");
-		System.out.println("Git test.");
-		
-		System.out.println("Git manipulation at Cmd.");
-		
 		System.out.println("Hangzhou,I'm coming.");
-		
 	}
-
 }
