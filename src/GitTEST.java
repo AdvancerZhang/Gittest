63c504a2661d73b9844de3d35e05de60c5b53155
AdvancerZhang <chenzhang0626@126.com>
2018-05-19 21:59:47 +0800

    Add one line.Print only.

--- src/GitTEST.java
+++ src/GitTEST.java
@@ -4,6 +4,8 @@ public class GitTEST {
 	public static void main(String[] args) {
 		System.out.println("Hello,Git.");
 		System.out.println("Nanjing.China");
+		System.out.println("Git test.");
+		
 	}
 
 }
